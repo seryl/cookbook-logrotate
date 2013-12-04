63b54d38d6936d6ff71593a54476d6f43bb45cd8
Josh Toft <josh@ooyala.com>
2013-12-04 09:40:38 -0800

    Added logrotate cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Josh Toft"
+maintainer_email "josh.toft@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures logrotate"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ providers/app.rb
@@ -0,0 +1,53 @@
+#
+# Author:: Josh Toft <josh.toft@gmail.com>
+# Cookbook Name:: logrotate
+# Provider:: app
+#
+# Copyright 2011, Josh Toft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :add do
+  path = new_resource.path.respond_to?(:each) ? new_resource.path : new_resource.path.split
+  
+  template "/etc/logrotate.d/#{new_resource.name}" do
+    source new_resource.template
+    cookbook new_resource.cookbook
+    mode 0440
+    owner "root"
+    group "root"
+    backup false
+    variables(
+      :name => new_resource.name,
+      :path => path,
+      :create => new_resource.create,
+      :frequency => new_resource.frequency,
+      :rotate => new_resource.rotate
+    )
+  end
+end
+
+action :remove do
+  path = new_resource.path.respond_to?(:each) ? new_resource.path : new_resource.path.split
+  
+  path.each do |p|
+    if ::File.exists?(p)
+      file p do
+        action :delete
+      end
+    end
+  end
+  
+  new_resource.updated_by_last_action(true)
+end
